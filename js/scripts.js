17a46f84190dd059b94e54b27a73979fc9818733
Charles Peden <ccbpeden@warpmail.net>
2017-01-17 10:37:17 -0800

    created nested for statements to replace all vowels in a string

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
 var sentenceArray = []
-var bigWordsArray = []
 
-var vowels = ["a", "e", "i", "o", "u" ]
+var vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "Y" ]
 
 $(document).ready(function() {
   $("#blanks").submit(function(event) {
@@ -9,7 +8,26 @@ $(document).ready(function() {
     var userInput = $("input#item1").val();
     console.log('The userInput is: "' + userInput + '"');
     if (userInput) {
-      
-    } else alert("There is no User Input")
+      var sentenceArray = [];
+      sentenceArray = userInput.split("");
+      console.log("The length of sentenceArray is " + sentenceArray.length)
+      var sentenceArrayIndex = 0;
+      var vowelIndex = 0;
+      for(vowelIndex = 0; vowelIndex < vowels.length; vowelIndex += 1) {
+        // alert(vowels[vowelIndex]);
+        for (sentenceArrayIndex = 0; sentenceArrayIndex <= sentenceArray.length; sentenceArrayIndex +=1) {
+          if (sentenceArray[sentenceArrayIndex] === vowels[vowelIndex]) {
+            sentenceArray[sentenceArrayIndex] = "-";
+          };
+        };
+      };
+      var userOutput = sentenceArray.join("");
+      console.log('The userOutput is: "' + userOutput + '"');
+      $("#resultsHere").append(userOutput);
+      $("#blanks").hide();
+      $("#results").show();
+    } else {
+      alert("There is no User Input");
+    };
   });
 });
